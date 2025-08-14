8d8cf1117aee01bfc295c87336ffe51a32e1e744
Yemi <yajibola23gmail.com>
2025-08-14 09:08:29 -0500

    ✨ feat(models): added minimal Play domain model

--- app/models/play.py
+++ app/models/play.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
 class Play:
-    def __init__(self, id: str, title: str, video_path: str) -> None:
-        self.id = id
-        self.title = title
-        self.video_path = video_path
+    id: str
+    title: str
+    video_path: str
